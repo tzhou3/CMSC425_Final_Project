411a609e790ca3ef17a87aa34f691b711b90ebcb
tzhou3 <tzhou535@gmail.com>
2019-11-22 14:52:51 -0500

    camera changes and player model changes

---
+++ CMSC425 Project/Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    public Vector3 offset;
+    public float zoomSpeed = 4f;
+    public float zoomMin = 5f;
+    public float zoomMax = 15f;
+    public float pitch = 2f;
+    public float currZoom = 10f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        currZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        currZoom = Mathf.Clamp(currZoom, zoomMin, zoomMax);
+    }
+
+    void LateUpdate()
+    {
+        transform.position = target.position - offset * currZoom;
+        transform.LookAt(target.position + Vector3.up * pitch);
+    }
+}
