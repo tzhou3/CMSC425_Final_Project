676cff97519abfa7968c826b4c96aada56bfdf6f
cblair28 <cblair28@terpmail.umd.edu>
2019-11-28 21:49:29 -0500

    updated scripts, added terrain hill

--- CMSC425 Project/Assets/Scripts/PlayerController.cs
+++ CMSC425 Project/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,11 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public float rotationSpeed;
     public float jump;
+    private float doubleJump;
+    private float GroundDistance;
     public float accel;
     private Rigidbody rb;
+    private CharacterController controller;
     public GameObject inventory;
     public Animator anim;
     private bool hasSled;
@@ -19,6 +22,10 @@ public class PlayerController : MonoBehaviour
         inventory.SetActive(false);
         anim = GetComponentInChildren<Animator>();
         isFinishedAnimation = true;
+        hasSled = true;
+        doubleJump = 0;
+        transform.Find("sleddingModel").gameObject.SetActive(false);
+        controller = GetComponent<CharacterController>();
     }
 
     IEnumerator JumpAnim()
@@ -37,6 +44,12 @@ public class PlayerController : MonoBehaviour
     }
     void Update()
     {
+
+        if (IsGrounded())
+        {
+            doubleJump = 0;
+        }
+ 
         if (Input.GetKeyDown(KeyCode.I))
         {
             if(inventory.activeSelf == true)
@@ -48,20 +61,37 @@ public class PlayerController : MonoBehaviour
                 inventory.SetActive(true);
             }
         }
-        if (!hasSled)
+        if (!hasSled | !Input.GetMouseButton(0))
         {
             
             if (Input.GetKeyDown(KeyCode.Space))
             {
 
-                //StartCoroutine(JumpAnim());
-                rb.AddForce(Vector3.up * 5f, ForceMode.Impulse);
+                doubleJump += 1;
+                if (doubleJump < 2)
+                {
+                    //StartCoroutine(JumpAnim());
+                    rb.AddForce(Vector3.up * jump, ForceMode.Impulse);
+                }
+
+          
             }
 
-            if (Input.GetAxisRaw("Vertical") != 0)
+            if ((Input.GetAxisRaw("Vertical") != 0))
             {
-                anim.SetBool("isWalking", true);
-                transform.position += transform.forward * speed * Time.deltaTime;
+                //character is jumping
+                if (!IsGrounded())
+                {
+                    anim.SetBool("isWalking", false);
+                    transform.position += transform.forward * speed/2 * Time.deltaTime;
+                }
+                //character is walking
+                else
+                {
+                    anim.SetBool("isWalking", true);
+                    transform.position += transform.forward * speed * Time.deltaTime;
+                }
+               
             }
             else
             {
@@ -75,40 +105,76 @@ public class PlayerController : MonoBehaviour
         // Get the horizontal and vertical axis.
         // By default they are mapped to the arrow keys.
         // The value is in the range -1 to 1
-        Vector3 translation = new Vector3(0,0,Input.GetAxis("Vertical")) * speed;
+        Vector3 translation = new Vector3(0, 0, Input.GetAxis("Vertical")) * speed;
         float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
         // Make it move 10 meters per second instead of 10 meters per frame...
         translation *= Time.deltaTime;
         rotation *= Time.deltaTime;
+        transform.Rotate(0, rotation, 0);
+
         if (hasSled) {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                rb.velocity = Vector3.up * jump;
-            }
-            // Rotate around our y-axis
+
             if (Input.GetMouseButton(0))
             {
+                transform.Find("riggedModel").gameObject.SetActive(false);
+                transform.Find("sleddingModel").gameObject.SetActive(true);
+                rb.freezeRotation = false;
+
                 rb.drag = 0.1f;
                 rb.AddForce(transform.forward * accel);
             }
-            else if (Input.GetKey(KeyCode.UpArrow))
+
+            else if (Input.GetMouseButtonUp(0))
             {
-                // Move translation along the object's z-axis
-                //transform.Translate(0, 0, translation);
-                rb.drag = 4;
-                rb.AddForce(transform.forward * speed);
+                transform.rotation = Quaternion.Euler(0, transform.eulerAngles.y, 0);
+                rb.constraints = RigidbodyConstraints.FreezeRotation;
+                transform.Find("riggedModel").gameObject.SetActive(true);
+                transform.Find("sleddingModel").gameObject.SetActive(false);
+
+        
+
+
             }
-            else if (Input.GetKey(KeyCode.DownArrow))
+            else
             {
-                // Move translation along the object's z-axis
-                //transform.Translate(0, 0, translation);
-                rb.drag = 4;
-                rb.AddForce(transform.forward * speed * -1);
+
+                //if (Input.GetKey(KeyCode.UpArrow))
+                //{
+                //    // Move translation along the object's z-axis
+                //    //transform.Translate(0, 0, translation);
+                //    rb.drag = 4;
+                //    rb.AddForce(transform.forward * speed);
+                //}
+                //if (Input.GetKey(KeyCode.DownArrow))
+                //{
+                //    // Move translation along the object's z-axis
+                //    //transform.Translate(0, 0, translation);
+                //    rb.drag = 4;
+                //    rb.AddForce(transform.forward * speed * -1);
+                //}
             }
+
         }
 
-        transform.Rotate(0, rotation, 0);
 
         
     }
+
+    bool IsGrounded()
+    {
+        RaycastHit hit;
+        float distance = 1f;
+        Vector3 dir = new Vector3(0, -1);
+
+        if (Physics.Raycast(transform.position, dir, out hit, distance))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
 }
+
