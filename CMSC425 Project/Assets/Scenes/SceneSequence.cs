994370db090077de50c9686e8c2a8a9f01d3f8b0
cblair28 <cblair28@terpmail.umd.edu>
2019-12-10 05:26:29 -0500

    finalized sounds/items/starts with cutscene

---
+++ CMSC425 Project/Assets/Scenes/SceneSequence.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSequence : MonoBehaviour
+{
+    public GameObject cam;
+    public GameObject mainCam;
+
+
+
+    void Start()
+    {
+        StartCoroutine(Cutscene());
+        mainCam.SetActive(true);
+    }
+
+    // Update is called once per frame
+    IEnumerator Cutscene()
+    {
+        yield return new WaitForSeconds(7);
+        mainCam.SetActive(true);
+        cam.SetActive(false);
+        
+    }
+}
